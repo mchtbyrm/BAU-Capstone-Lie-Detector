513b00079d36c7708d3cdda3504900a16e4c7b6c
mcht <m.byrm45@icloud.com>
2023-04-16 16:49:15 +0300

    Some changes added

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from Models.logistic_regression import logistic_regression
 from Models.kNN import k_nearest_neighbors, choose_k
 from Models.naive_bayes import gaussian_naive_bayes
 from Models.support_vector_machines import support_vector_machines, choose_parameters
+from Models.trees import decision_tree, random_forest, choose_n_estimators
 from Models.evaluation import evaluate_model
 
 
@@ -49,29 +50,35 @@ print(df_scaled.head())
 
 # model, predictions = gaussian_naive_bayes(x_train, y_train, x_test)
 
-best_params = choose_parameters(x_train, y_train)
+# best_params = choose_parameters(x_train, y_train)
+#
+# model, predictions = support_vector_machines(x_train, y_train, x_test, best_params['C'], best_params['gamma'],
+#                                              best_params['kernel'])
 
-model, predictions = support_vector_machines(x_train, y_train, x_test, best_params['C'], best_params['gamma'],
-                                             best_params['kernel'])
+model, predictions = decision_tree(x_train, y_train, x_test)
+
+# best_n_estimators = choose_n_estimators(x_train, y_train, x_test, y_test)
+#
+# model, predictions = random_forest(x_train, y_train, x_test, best_n_estimators)
 
 evaluate_model(predictions, y_test)
 
 # read_serial()
 
-bpm_data, gsr_data = read_data('COM1', 60)
-print(bpm_data)
-print(gsr_data)
-features = calculate_features(bpm_data, gsr_data)
-features = features.reshape(1, -1)
-features_scaled = scale_data(features)
-print(features)
-
-print("*****************************************************************")
-
-if model.predict(features) == 1:
-    print("The subject is telling the truth")
-else:
-    print("The subject is lying")
+# bpm_data, gsr_data = read_data('COM1', 60)
+# print(bpm_data)
+# print(gsr_data)
+# features = calculate_features(bpm_data, gsr_data)
+# features = features.reshape(1, -1)
+# features_scaled = scale_data(features)
+# print(features)
+#
+# print("*****************************************************************")
+#
+# if model.predict(features) == 1:
+#     print("The subject is telling the truth")
+# else:
+#     print("The subject is lying")
 
 
 
